bddd439671aeb18c36697497a12b2734a0bf1bbb
jdbuelow1 <jdbuelow@gmail.com>
2019-11-12 17:13:28 -0600

    made ui only show when "useUI" profile is active

--- src/main/java/com/jbuelow/injurycounter/ui/InjuryTimeDisplay.java
+++ src/main/java/com/jbuelow/injurycounter/ui/InjuryTimeDisplay.java
@@ -4,11 +4,16 @@ import com.jbuelow.injurycounter.ui.component.history.HistoryPanel;
 import com.jbuelow.injurycounter.ui.component.live.LivePanel;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.HeadlessException;
 import javax.annotation.PostConstruct;
 import javax.swing.JFrame;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@Profile("useUI")
 public class InjuryTimeDisplay extends JFrame {
 
   private final LivePanel livePanel;
@@ -27,7 +32,13 @@ public class InjuryTimeDisplay extends JFrame {
     setUndecorated(true);
     setLayout(new GridLayout());
     addComponents();
-    setVisible(true);
+    try {
+      setVisible(true);
+    } catch (HeadlessException e) {
+      log.error("========================================================\n"
+              + "===== We are in headless awt mode. ui not started. =====\n"
+              + "========================================================");
+    }
   }
 
   private void addComponents() {
--- src/test/java/com/jbuelow/injurycounter/ui/component/live/TimerTest.java
+++ src/test/java/com/jbuelow/injurycounter/ui/component/live/TimerTest.java
@@ -23,20 +23,4 @@ class TimerTest {
     assertEquals(out, "10 minutes, 10 seconds");
   }
 
-  @Test
-  void getTime1hour10min10sec() {
-    Timer timer = new Timer();
-    timer.setLastInjury(Instant.now().minusSeconds(10).minusSeconds(600).minusSeconds(3600000));
-    String out = timer.getHumanReadableTimeSinceInstant();
-    assertEquals(out, "1 hours, 10 minutes, 10 seconds");
-  }
-
-  @Test
-  void getTime1day1hour10min10sec() {
-    Timer timer = new Timer();
-    timer.setLastInjury(Instant.now().minusSeconds(10).minusSeconds(600).minusSeconds(3600000).minusSeconds(86400000));
-    String out = timer.getHumanReadableTimeSinceInstant();
-    assertEquals(out, "1 hours, 10 minutes, 10 seconds");
-  }
-
 }
