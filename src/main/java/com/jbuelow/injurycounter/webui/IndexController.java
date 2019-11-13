02634a143b26655110178d0cf13c7ed713776860
jdbuelow1 <jdbuelow@gmail.com>
2019-11-13 13:58:40 -0600

    added controller to serve index page

---
+++ src/main/java/com/jbuelow/injurycounter/webui/IndexController.java
@@ -0,0 +1,15 @@
+package com.jbuelow.injurycounter.webui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/")
+  public String showIndex(Model model) {
+    return "index";
+  }
+
+}
