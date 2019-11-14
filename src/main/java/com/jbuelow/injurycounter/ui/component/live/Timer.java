cfb7c258c11ab1fab1cf7bf7df16656895fca55f
jdbuelow1 <jdbuelow@gmail.com>
2019-11-14 13:12:22 -0600

    made time formatting method static to be used from JFX app

--- src/main/java/com/jbuelow/injurycounter/ui/component/live/Timer.java
+++ src/main/java/com/jbuelow/injurycounter/ui/component/live/Timer.java
@@ -29,13 +29,13 @@ public class Timer extends JLabel {
 
   @Override
   public void paintComponent(Graphics g) {
-    String text = getHumanReadableTimeSinceInstant();
+    String text = getHumanReadableTimeSinceInstant(lastInjury);
     setText(text);
     log.trace("Setting new timer text to {}", text);
     super.paintComponent(g);
   }
 
-  public String getHumanReadableTimeSinceInstant() {
+  public static String getHumanReadableTimeSinceInstant(Instant lastInjury) {
     Instant now = Instant.now();
     Duration duration = Duration.between(lastInjury, now);
     long mills = duration.toMillis();
