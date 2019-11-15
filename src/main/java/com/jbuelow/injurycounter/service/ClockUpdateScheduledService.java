1b4ab45db3a79071220fcc6bcedf453ef830d7d7
jdbuelow1 <jdbuelow@gmail.com>
2019-11-15 12:34:31 -0600

    Revert "made clock updater work"
    This reverts commit e39139f5

--- src/main/java/com/jbuelow/injurycounter/service/ClockUpdateScheduledService.java
+++ src/main/java/com/jbuelow/injurycounter/service/ClockUpdateScheduledService.java
@@ -2,8 +2,6 @@ package com.jbuelow.injurycounter.service;
 
 import com.jbuelow.injurycounter.fxui.FxUiController;
 import com.jbuelow.injurycounter.ui.component.live.Timer;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
@@ -27,15 +25,9 @@ public class ClockUpdateScheduledService {
     this.fxUiController = fxUiController;
   }
 
-  @Scheduled(fixedRate = 500)
+  @Scheduled(fixedRate = 1000)
   public void updateDateTimeClock() {
-    Label l = fxUiController.getDateTime();
-    if (l == null) {
-      log.warn("Label object is null. Maybe the UI has not loaded yet?");
-      return;
-    }
-    String dateTime = new SimpleDateFormat("MM/dd/yyyy  hh:mm:ss aa").format(Timestamp.from(Instant.now()));
-    Platform.runLater(() -> l.setText(dateTime));
+
   }
 
   @Scheduled(fixedRate = 20)
