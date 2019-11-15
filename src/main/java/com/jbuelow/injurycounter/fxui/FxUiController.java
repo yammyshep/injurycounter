416afe3f873ca458561e018a6a61c7cd7ccd1aa1
jdbuelow1 <jdbuelow@gmail.com>
2019-11-15 12:34:41 -0600

    Revert "added getters and updateInjury method"
    This reverts commit 5a5d2a28

--- src/main/java/com/jbuelow/injurycounter/fxui/FxUiController.java
+++ src/main/java/com/jbuelow/injurycounter/fxui/FxUiController.java
@@ -1,20 +1,16 @@
 package com.jbuelow.injurycounter.fxui;
 
-import com.jbuelow.injurycounter.data.entity.Injury;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FxUiController {
 
   @FXML
-  @Getter
   private Label dateTime;
 
   @FXML
-  @Getter
   private Label timeSinceInjury;
 
   @FXML
@@ -22,10 +18,5 @@ public class FxUiController {
 
   @FXML
   private Label injuryDescription;
-
-  public void updateInjury(Injury newInjury) {
-    injuredPersonName.setText(newInjury.getPerson().getName());
-    injuryDescription.setText(newInjury.getDescription());
-  }
 
 }
