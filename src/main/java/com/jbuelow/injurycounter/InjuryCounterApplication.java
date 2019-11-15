2f24973170ce8f3bd4e5c8628cbf6928101f5c72
jdbuelow1 <jdbuelow@gmail.com>
2019-11-15 12:35:53 -0600

    Revert "replaced main application launcher class with one that starts JFX with spring"
    This reverts commit 4490f5c7

---
+++ src/main/java/com/jbuelow/injurycounter/InjuryCounterApplication.java
@@ -0,0 +1,13 @@
+package com.jbuelow.injurycounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjuryCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjuryCounterApplication.class, args);
+	}
+
+}
