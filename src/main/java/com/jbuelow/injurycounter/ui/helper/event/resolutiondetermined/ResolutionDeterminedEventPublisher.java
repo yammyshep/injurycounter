863135f17237936289114cad67f6c7688ba1dcf0
jdbuelow1 <jdbuelow@gmail.com>
2019-11-20 13:29:04 -0600

    made it not restart the program but rather override the resolution with 1080p

--- src/main/java/com/jbuelow/injurycounter/ui/helper/event/resolutiondetermined/ResolutionDeterminedEventPublisher.java
+++ src/main/java/com/jbuelow/injurycounter/ui/helper/event/resolutiondetermined/ResolutionDeterminedEventPublisher.java
@@ -1,6 +1,7 @@
 package com.jbuelow.injurycounter.ui.helper.event.resolutiondetermined;
 
 import com.jbuelow.injurycounter.ui.helper.DisplaySizing;
+import java.awt.Dimension;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -17,12 +18,11 @@ public class ResolutionDeterminedEventPublisher {
   }
 
   public void publish(DisplaySizing displaySizing) {
-    log.info("Publishing dynamic display resize event with resolution of {}p", displaySizing.displayPercent(100));
     if (displaySizing.displayPercent(100)<100) {
-      log.error("Screen size of {} is not valid", displaySizing.displayPercent(100));
-      log.error("Restarting...");
-      System.exit(0);
+      log.warn("Screen size of {}p is not valid! Assuming new size of 1080p", displaySizing.displayPercent(100));
+      displaySizing.setDisplayDimension(new Dimension(1920,1080));
     }
+    log.info("Publishing dynamic display resize event with resolution of {}p", displaySizing.displayPercent(100));
     ResolutionDeterminedEvent e = new ResolutionDeterminedEvent(this, displaySizing);
     applicationEventPublisher.publishEvent(e);
   }
