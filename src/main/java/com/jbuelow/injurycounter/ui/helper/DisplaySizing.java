1692e93fedff2d859acdd320623986feb9ce4ca9
jdbuelow1 <jdbuelow@gmail.com>
2019-11-22 13:40:40 -0600

    added image resize method

--- src/main/java/com/jbuelow/injurycounter/ui/helper/DisplaySizing.java
+++ src/main/java/com/jbuelow/injurycounter/ui/helper/DisplaySizing.java
@@ -1,6 +1,10 @@
 package com.jbuelow.injurycounter.ui.helper;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import lombok.Setter;
 
 public class DisplaySizing {
@@ -14,6 +18,17 @@ public class DisplaySizing {
     int h = displayDimension.height;
     float p = percent / 100;
     return (int) (h*p);
+  }
+
+  private static Image getScaledImage(Image srcImg, int w, int h){
+    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = resizedImg.createGraphics();
+
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g2.drawImage(srcImg, 0, 0, w, h, null);
+    g2.dispose();
+
+    return resizedImg;
   }
 
 }
