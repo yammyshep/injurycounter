8c004f00b9189ee4df95c5455573a0d7540e9079
jdbuelow1 <jdbuelow@gmail.com>
2019-12-16 11:16:18 -0600

    added controller to manage requests to modify/add users

---
+++ src/main/java/com/jbuelow/injurycounter/webui/form/ModifyUserController.java
@@ -0,0 +1,56 @@
+package com.jbuelow.injurycounter.webui.form;
+
+import com.jbuelow.injurycounter.data.entity.Person;
+import com.jbuelow.injurycounter.data.repo.PersonRepository;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ModifyUserController {
+
+  private final PersonRepository personRepo;
+
+  public ModifyUserController(PersonRepository personRepo) {
+    this.personRepo = personRepo;
+  }
+
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+  }
+
+  @GetMapping("/user")
+  public String getUserForm(Model model, @RequestParam(required = false) Long id) {
+    if (Objects.isNull(id)) {
+      model.addAttribute("command", new Person());
+    } else {
+      Optional<Person> p = personRepo.findById(id);
+      if (p.isPresent()) {
+        model.addAttribute("userFound", true);
+        model.addAttribute("command", p.get());
+      } else {
+        model.addAttribute("userNotFound", true);
+        model.addAttribute("command", new Person());
+      }
+    }
+
+    return "addUser";
+  }
+
+  @PostMapping("/user")
+  public String postModifiedUser(Model model, Person person) {
+    personRepo.save(person);
+    model.addAttribute("command", person);
+    model.addAttribute("submitted", true);
+    return "addUser";
+  }
+
+}
