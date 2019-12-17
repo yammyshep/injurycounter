7c346fd7ebc66f5a29812ced4e1593cd5656d6f9
jdbuelow1 <jdbuelow@gmail.com>
2019-12-17 13:08:18 -0600

    added api endpoints for dumping teams

--- src/main/java/com/jbuelow/injurycounter/api/database/DumpTableController.java
+++ src/main/java/com/jbuelow/injurycounter/api/database/DumpTableController.java
@@ -3,9 +3,11 @@ package com.jbuelow.injurycounter.api.database;
 import com.jbuelow.injurycounter.data.entity.AccessLog;
 import com.jbuelow.injurycounter.data.entity.Injury;
 import com.jbuelow.injurycounter.data.entity.Person;
+import com.jbuelow.injurycounter.data.entity.Team;
 import com.jbuelow.injurycounter.data.repo.AccessLogRepository;
 import com.jbuelow.injurycounter.data.repo.InjuryRepository;
 import com.jbuelow.injurycounter.data.repo.PersonRepository;
+import com.jbuelow.injurycounter.data.repo.TeamRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,13 +20,15 @@ public class DumpTableController {
   private final InjuryRepository injuryRepo;
   private final PersonRepository personRepo;
   private final AccessLogRepository accessRepo;
+  private final TeamRepository teamRepo;
 
   public DumpTableController(InjuryRepository injuryRepo,
       PersonRepository personRepo,
-      AccessLogRepository accessRepo) {
+      AccessLogRepository accessRepo, TeamRepository teamRepo) {
     this.injuryRepo = injuryRepo;
     this.personRepo = personRepo;
     this.accessRepo = accessRepo;
+    this.teamRepo = teamRepo;
   }
 
   @GetMapping("/injury")
@@ -40,6 +44,11 @@ public class DumpTableController {
   @GetMapping("/access")
   public @ResponseBody Iterable<AccessLog> dumpAccessLog() {
     return accessRepo.findAll();
+  }
+
+  @GetMapping("/team")
+  public @ResponseBody Iterable<Team> dumpTeams() {
+    return teamRepo.findAll();
   }
 
 }
