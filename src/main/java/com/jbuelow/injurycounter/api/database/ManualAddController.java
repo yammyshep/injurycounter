1c850549f1730399895adb1f2440ae6923836099
jdbuelow1 <jdbuelow@gmail.com>
2019-12-17 13:06:20 -0600

    added manual add api endpoints for creating teams

--- src/main/java/com/jbuelow/injurycounter/api/database/ManualAddController.java
+++ src/main/java/com/jbuelow/injurycounter/api/database/ManualAddController.java
@@ -3,9 +3,11 @@ package com.jbuelow.injurycounter.api.database;
 import com.jbuelow.injurycounter.data.entity.AccessLog;
 import com.jbuelow.injurycounter.data.entity.Injury;
 import com.jbuelow.injurycounter.data.entity.Person;
+import com.jbuelow.injurycounter.data.entity.Team;
 import com.jbuelow.injurycounter.data.repo.AccessLogRepository;
 import com.jbuelow.injurycounter.data.repo.InjuryRepository;
 import com.jbuelow.injurycounter.data.repo.PersonRepository;
+import com.jbuelow.injurycounter.data.repo.TeamRepository;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,19 +22,19 @@ public class ManualAddController {
   private final PersonRepository personRepo;
   private final InjuryRepository injuryRepo;
   private final AccessLogRepository accessRepo;
+  private final TeamRepository teamRepo;
 
   public ManualAddController(PersonRepository personRepo,
       InjuryRepository injuryRepo,
-      AccessLogRepository accessRepo) {
+      AccessLogRepository accessRepo, TeamRepository teamRepo) {
     this.personRepo = personRepo;
     this.injuryRepo = injuryRepo;
     this.accessRepo = accessRepo;
+    this.teamRepo = teamRepo;
   }
 
   @PostMapping("/person")
   public @ResponseBody Person addPerson(HttpServletRequest httpReq, @RequestBody Person person) {
-    assert person.getId() != null;
-    AccessLog al = new AccessLog();
     personRepo.save(person);
     accessRepo.save(AccessLog.forPerson(person, httpReq));
     return person;
@@ -40,10 +42,16 @@ public class ManualAddController {
 
   @PostMapping("/injury")
   public @ResponseBody Injury addInjury(HttpServletRequest httpReq, @RequestBody Injury injury) {
-    assert injury.getPerson() != null;
     injuryRepo.save(injury);
     accessRepo.save(AccessLog.forInjury(injury, httpReq));
     return injury;
+  }
+
+  @PostMapping("/team")
+  public @ResponseBody Team addTeam(HttpServletRequest httpReq, @RequestBody Team team) {
+    teamRepo.save(team);
+    accessRepo.save(AccessLog.forTeam(team, httpReq));
+    return team;
   }
 
 }
