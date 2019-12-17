aabc10d88737fc35695712f3b094e9fab96cb876
jdbuelow1 <jdbuelow@gmail.com>
2019-12-17 13:49:16 -0600

    added test to assert the team selection field on the ModifyUser view

--- src/test/java/com/jbuelow/injurycounter/webui/form/ModifyUserControllerTest.java
+++ src/test/java/com/jbuelow/injurycounter/webui/form/ModifyUserControllerTest.java
@@ -6,7 +6,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 import com.jbuelow.injurycounter.data.entity.Person;
+import com.jbuelow.injurycounter.data.entity.Team;
 import com.jbuelow.injurycounter.data.repo.PersonRepository;
+import com.jbuelow.injurycounter.data.repo.TeamRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,6 +33,9 @@ public class ModifyUserControllerTest {
 
   @Autowired
   private PersonRepository personRepo;
+
+  @Autowired
+  private TeamRepository teamRepo;
 
   private MockMvc mvc;
 
@@ -60,7 +65,8 @@ public class ModifyUserControllerTest {
         .andExpect(xpath("//form//input[@type='text'][@name='gender'][@required]").doesNotExist())
         .andExpect(xpath("//form//input[@type='date'][@name='dob']").exists())
         .andExpect(xpath("//form//input[@type='date'][@name='dob'][@required]").doesNotExist())
-        .andExpect(xpath("//form//input[@type='checkbox'][@name='staff'][@readonly]").exists())
+        .andExpect(xpath("//form//select").exists())
+        .andExpect(xpath("//form//select/option[@value='']").exists())
         .andExpect(xpath("//form//input[@type='submit']").exists())
         .andDo(print());
   }
@@ -92,7 +98,8 @@ public class ModifyUserControllerTest {
         .andExpect(xpath("//form//input[@type='text'][@name='gender'][@required]").doesNotExist())
         .andExpect(xpath("//form//input[@type='date'][@name='dob']").exists())
         .andExpect(xpath("//form//input[@type='date'][@name='dob'][@required]").doesNotExist())
-        .andExpect(xpath("//form//input[@type='checkbox'][@name='staff'][@readonly]").exists())
+        .andExpect(xpath("//form//select").exists())
+        .andExpect(xpath("//form//select/option[@value='']").exists())
         .andExpect(xpath("//form//input[@type='submit']").exists())
         .andExpect(xpath("//form//input[@type='number'][@name='id'][@required][@readonly][@value='1234']").exists())
         .andExpect(xpath("//form//input[@type='text'][@name='name'][@required][@value='Jose Galegrekowsi']").exists())
@@ -102,6 +109,43 @@ public class ModifyUserControllerTest {
 
     log.debug("Cleaning up database...");
     personRepo.deleteAll();
+  }
+
+  @Test
+  void getFormWithTeams() throws Exception {
+    log.debug("Creating test teams...");
+    Team t1 = new Team();
+    t1.setName("First Team");
+    Team t2 = new Team();
+    t2.setName("Second Team");
+    teamRepo.save(t1);
+    teamRepo.save(t2);
+
+    log.debug("Fetching user form...");
+    MockHttpServletRequestBuilder rb = MockMvcRequestBuilders.get("/user");
+    rb.accept(MediaType.TEXT_HTML);
+    mvc.perform(rb)
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(xpath("/html").exists())
+        .andExpect(xpath("/html/head").exists())
+        .andExpect(xpath("/html/body").exists())
+        .andExpect(xpath("//form[@method='POST'][@action='/user']").exists())
+        .andExpect(xpath("//form//input[@type='number'][@name='id'][@required]").exists())
+        .andExpect(xpath("//form//input[@type='number'][@name='id'][@required][@readonly]").doesNotExist())
+        .andExpect(xpath("//form//button[@type='submit'][@formmethod='get'][@formnovalidate]").exists())
+        .andExpect(xpath("//form//input[@type='text'][@name='name'][@required]").exists())
+        .andExpect(xpath("//form//input[@type='text'][@name='shortName']").exists())
+        .andExpect(xpath("//form//input[@type='number'][@name='grade']").exists())
+        .andExpect(xpath("//form//input[@type='text'][@name='gender']").exists())
+        .andExpect(xpath("//form//input[@type='text'][@name='gender'][@required]").doesNotExist())
+        .andExpect(xpath("//form//input[@type='date'][@name='dob']").exists())
+        .andExpect(xpath("//form//input[@type='date'][@name='dob'][@required]").doesNotExist())
+        .andExpect(xpath("//form//select").exists())
+        .andExpect(xpath("//form//select/option[@value='']").exists())
+        .andExpect(xpath("//form//select/option[@value='"+t1.getId()+"']").string("First Team"))
+        .andExpect(xpath("//form//select/option[@value='"+t2.getId()+"']").string("Second Team"))
+        .andExpect(xpath("//form//input[@type='submit']").exists())
+        .andDo(print());
   }
 
   @Test
@@ -126,7 +170,8 @@ public class ModifyUserControllerTest {
         .andExpect(xpath("//form//input[@type='text'][@name='gender'][@required]").doesNotExist())
         .andExpect(xpath("//form//input[@type='date'][@name='dob']").exists())
         .andExpect(xpath("//form//input[@type='date'][@name='dob'][@required]").doesNotExist())
-        .andExpect(xpath("//form//input[@type='checkbox'][@name='staff'][@readonly]").exists())
+        .andExpect(xpath("//form//select").exists())
+        .andExpect(xpath("//form//select/option[@value='']").exists())
         .andExpect(xpath("//form//input[@type='submit']").exists())
         .andExpect(xpath("//form//input[@type='number'][@name='id'][@required][@readonly][@value='1234']").exists())
         .andExpect(xpath("//form//input[@type='text'][@name='name'][@required][@value='Jose Galegrekowsi']").exists())
