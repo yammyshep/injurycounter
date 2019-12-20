7fafed612c2c0c44977859a9b752c7c408a53f41
jdbuelow1 <jdbuelow@gmail.com>
2019-12-20 11:35:27 -0600

    added label to panel, fixed value annotation, and implemented time since last string replacment vars

--- src/main/java/com/jbuelow/injurycounter/ui/popup/newinjury/compnent/FooterPanel.java
+++ src/main/java/com/jbuelow/injurycounter/ui/popup/newinjury/compnent/FooterPanel.java
@@ -2,8 +2,10 @@ package com.jbuelow.injurycounter.ui.popup.newinjury.compnent;
 
 import com.jbuelow.injurycounter.data.entity.Injury;
 import com.jbuelow.injurycounter.event.injuryupdate.InjuryUpdateEvent;
+import com.jbuelow.injurycounter.ui.component.live.Timer;
 import com.jbuelow.injurycounter.ui.helper.DisplaySizing;
 import com.jbuelow.injurycounter.ui.helper.event.resolutiondetermined.ResolutionDeterminedEvent;
+import java.awt.BorderLayout;
 import java.awt.Font;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,17 +14,19 @@ import javax.annotation.PostConstruct;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.text.StrSubstitutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile("useUI")
 public class FooterPanel extends JPanel {
 
-  @Value("${injury-counter.new-injury.footer")
+  @Value("${injury-counter.new-injury.footer}")
   private String footerText;
 
   private final JLabel footerLabel = new JLabel("", SwingConstants.CENTER);
@@ -30,14 +34,18 @@ public class FooterPanel extends JPanel {
   @PostConstruct
   public void init() {
     footerLabel.setText(footerText);
+    setLayout(new BorderLayout());
+    add(footerLabel, BorderLayout.CENTER);
   }
 
-  public void setForInjury(Injury injury) {
+  public void setForInjury(Injury injury, Injury lastInjury) {
     Map<String, String> dataMap = new HashMap<>();
     dataMap.put("has/have", Objects.isNull(injury.getInstigator())? "has":"have");
-    dataMap.put("prevDays", "[NOT_IMPLEMENTED]");
-    dataMap.put("prevTime", "[NOT_IMPLEMENTED]"); //TODO implement this
-    footerLabel.setText(StrSubstitutor.replace(footerText, dataMap));
+    String timeSince = Timer.getHumanReadableDiff(lastInjury.getTimestamp().toInstant());
+    timeSince = timeSince.replaceFirst("<br>", ", ");
+    dataMap.put("prevTime", timeSince);
+    String parsed = StrSubstitutor.replace(footerText, dataMap, "&{", "}");
+    footerLabel.setText(parsed);
   }
 
   @Component
@@ -46,8 +54,9 @@ public class FooterPanel extends JPanel {
 
     @Override
     public void onApplicationEvent(ResolutionDeterminedEvent resolutionDeterminedEvent) {
+      log.debug("Got ResolutionDeterminedEvent.");
       DisplaySizing ds = resolutionDeterminedEvent.getSizing();
-      footerLabel.setFont(new Font(footerLabel.getFont().getName(), footerLabel.getFont().getStyle(), ds.displayPercent(8)));
+      footerLabel.setFont(new Font(footerLabel.getFont().getName(), footerLabel.getFont().getStyle(), ds.displayPercent(6)));
     }
 
   }
@@ -58,8 +67,8 @@ public class FooterPanel extends JPanel {
 
     @Override
     public void onApplicationEvent(InjuryUpdateEvent injuryUpdateEvent) {
-      Injury injury = injuryUpdateEvent.getInjury();
-      setForInjury(injury);
+      log.debug("Got InjuryUpdateEvent.");
+      setForInjury(injuryUpdateEvent.getInjury(), injuryUpdateEvent.getLastInjury());
     }
 
   }
