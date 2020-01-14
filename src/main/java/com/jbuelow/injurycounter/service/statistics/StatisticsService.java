bc3f0a0e334de548f751e3e474d8eaf58154af90
jdbuelow1 <jdbuelow@gmail.com>
2020-01-14 14:22:34 -0600

    removed async enable annotation

--- src/main/java/com/jbuelow/injurycounter/service/statistics/StatisticsService.java
+++ src/main/java/com/jbuelow/injurycounter/service/statistics/StatisticsService.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -21,7 +20,6 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @EnableScheduling
-@EnableAsync
 public class StatisticsService implements ApplicationContextAware {
 
   @Setter
