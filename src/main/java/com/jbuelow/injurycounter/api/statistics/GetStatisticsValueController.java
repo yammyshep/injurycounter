553ecdceae0a144c52251d01bdefe96797b48ca9
jdbuelow1 <jdbuelow@gmail.com>
2020-01-15 10:34:52 -0600

    made api endpoint throw a more sensable exception

--- src/main/java/com/jbuelow/injurycounter/api/statistics/GetStatisticsValueController.java
+++ src/main/java/com/jbuelow/injurycounter/api/statistics/GetStatisticsValueController.java
@@ -2,9 +2,11 @@ package com.jbuelow.injurycounter.api.statistics;
 
 import com.jbuelow.injurycounter.service.statistics.StatisticsService;
 import com.jbuelow.injurycounter.service.statistics.component.StatComponent;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/stat")
@@ -19,9 +21,19 @@ public class GetStatisticsValueController {
   }
 
   @GetMapping("/value/{statClass}")
-  public Object getValue(@PathVariable String statClass) throws ClassNotFoundException {
-    StatComponent comp = statService.getStat(Class.forName(statClass));
+  public Object getValue(@PathVariable String statClass) {
+    StatComponent comp = null;
+    try {
+      comp = statService.getStat(Class.forName(statClass));
+    } catch (ClassNotFoundException e) {
+      throw new StatNotFoundException();
+    }
     return comp.getValue();
+  }
+
+  @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Stat definition not found")
+  public class StatNotFoundException extends RuntimeException {
+
   }
 
 }
