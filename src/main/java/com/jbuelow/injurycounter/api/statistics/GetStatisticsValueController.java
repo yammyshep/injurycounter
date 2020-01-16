92d38dc8e2a72497a69eaee86e03375b5e2afe46
jdbuelow1 <jdbuelow@gmail.com>
2020-01-16 13:44:01 -0600

    redid statistics api controller for compatability with new backend

--- src/main/java/com/jbuelow/injurycounter/api/statistics/GetStatisticsValueController.java
+++ src/main/java/com/jbuelow/injurycounter/api/statistics/GetStatisticsValueController.java
@@ -1,8 +1,11 @@
 package com.jbuelow.injurycounter.api.statistics;
 
 import com.jbuelow.injurycounter.service.statistics.StatisticsService;
-import com.jbuelow.injurycounter.service.statistics.component.StatComponent;
+import com.jbuelow.injurycounter.service.statistics.data.Statistic;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,19 +25,26 @@ public class GetStatisticsValueController {
   }
 
   @GetMapping("/list")
-  public List<Class> getClassifierListing() {
-    return statService.getStats();
+  public List<Class<?>> getClassifierListing() {
+    return new ArrayList<>(statService.getHandlers());
   }
 
   @GetMapping("/value/{statClass}")
-  public Object getValue(@PathVariable String statClass) {
-    StatComponent comp = null;
+  public Statistic getValue(@PathVariable String statClass) {
+    Statistic s = null;
     try {
-      comp = statService.getStat(Class.forName(statClass));
+      s = statService.getResult((Class<? extends Statistic>) Class.forName(statClass));
     } catch (ClassNotFoundException e) {
       throw new StatNotFoundException();
     }
-    return comp.getValue();
+    return s;
+  }
+
+  @GetMapping("/value")
+  public Map<Class<? extends Statistic>, Statistic> getAllValues() {
+    Map<Class<? extends Statistic>, Statistic> stats = new HashMap<>();
+    statService.getHandlers().forEach(h -> stats.put(h, statService.getResult(h)));
+    return stats;
   }
 
   @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Stat definition not found")
